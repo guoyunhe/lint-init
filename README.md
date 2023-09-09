dc42e75d6cfb1ef83ee4444e09589058adf7a59c
Guo Yunhe <i@guoyunhe.me>
2023-09-09 18:20:32 +0800

    options

--- README.md
+++ README.md
@@ -1,48 +1,7 @@
 # lint-init
 
-## Install
-
-### Run directly
+Initialize ESLint, Stylelint, Prettier, EditorConfig for your projects
 
 ```bash
 npx lint-init
 ```
-
-### Install on system
-
-```bash
-npm i -g lint-init
-%binName%
-```
-
-### Install in project
-
-```bash
-npm i -D lint-init
-```
-
-Add script entry:
-
-```json
-{
-  "scripts": {
-    "%binName%": "%binName%"
-  }
-}
-```
-
-Run:
-
-```bash
-npm run %binName%
-```
-
-## Options
-
-### `--help`
-
-Show help
-
-### `--version`
-
-Show version
--- src/bin/lint-init.ts
+++ src/bin/lint-init.ts
@@ -1,22 +1,46 @@
 #!/usr/bin/env node
 
-import { intro, outro } from '@clack/prompts';
+import { intro, multiselect, outro, select } from '@clack/prompts';
 import chalk from 'chalk';
 import enMessages from '../i18n/en.json';
 import zhMessages from '../i18n/zh.json';
 
-const messages = process.env['LANG']?.startsWith('zh') ? zhMessages : enMessages;
+(async () => {
+  const messages = process.env['LANG']?.startsWith('zh') ? zhMessages : enMessages;
 
-console.log('');
+  console.log('');
 
-intro(
-  '🚀 ' +
-    chalk.bold(chalk.cyan(PACKAGE_NAME)) +
-    ' ' +
-    chalk.dim('v' + PACKAGE_VERSION) +
-    ' by guoyunhe'
-);
+  intro(
+    '🚀 ' +
+      chalk.bold(chalk.cyan(PACKAGE_NAME)) +
+      ' ' +
+      chalk.dim('v' + PACKAGE_VERSION) +
+      ' by guoyunhe'
+  );
 
-outro(
-  '🎉 ' + messages.thank + '\n      ' + chalk.underline('https://github.com/guoyunhe/lint-init')
-);
+  const linters = await multiselect({
+    message: messages.linters,
+    options: [
+      { value: 'eslint', label: 'ESLint', hint: 'JS, TS, React, Vue' },
+      { value: 'stylelint', label: 'Stylelint', hint: 'CSS, SCSS, LESS' },
+      {
+        value: 'prettier',
+        label: 'Prettier',
+        hint: 'HTML, Markdown, CSS, SCSS, LESS, JS, TS, React, Vue',
+      },
+    ],
+    required: true,
+  });
+
+  const ci = await select({
+    message: messages.ci,
+    options: [
+      { value: 'github-action', label: 'GitHub Action' },
+      { value: 'gitlab-ci', label: 'GitLab CI' },
+    ],
+  });
+
+  outro(
+    '🎉 ' + messages.thank + '\n      ' + chalk.underline('https://github.com/guoyunhe/lint-init')
+  );
+})();

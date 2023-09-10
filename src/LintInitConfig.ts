cb48895d77b725ca24034cc7ff20f91e5a2994aa
Guo Yunhe <i@guoyunhe.me>
2023-09-10 21:05:23 +0800

    fix: type

--- src/LintInitConfig.ts
+++ src/LintInitConfig.ts
@@ -89,7 +89,7 @@ export interface StylelintInitPreset {
    *
    * @default 'package.json'
    */
-  configFile: string | string[];
+  configFile?: string | string[];
   /**
    * Content of `.stylelintignore` file.
    *
